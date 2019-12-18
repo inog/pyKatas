d046f940bf6e2237a473ee450a460eb2a17cdb81
inog <kontakt@ingo-reschke.de>
2019-12-18 23:26:17 +0100

    Initial commit

---
+++ variables.py
@@ -0,0 +1,8 @@
+name = "John"
+age = "50"
+isMale = True
+
+print("There once was a man named " + name + ", ")
+print("he was " + age + "years old- ")
+print("He really liked the name " + name +", ")
+print("but did't like being "+ age + ".")
