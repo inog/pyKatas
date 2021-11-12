36c526dc5f67685754afe0db0716c942aceafb12
Ingo Reschke <kontakt@ingo-reschke.de>
2021-11-12 22:27:30 +0100

    round

--- HourlyWageCalculator.py
+++ HourlyWageCalculator.py
@@ -17,8 +17,8 @@ def calcu(yearly, hoursPerWeek):
     monthly = yearly / 12
     return  3 * monthly / 13 / hoursPerWeek
 
-yearlyList = [60000, 52500,67000,70000,90000,100000, 110000]
-weekHours = [40.0, 30.0, 37.5, 40.0, 40.0, 42.0, 42.0]
+yearlyList = [60000, 52500,67000,70000,90000,100000,100000, 110000]
+weekHours = [40.0, 30.0, 37.5, 40.0, 40.0, 40.0, 42.0, 42.0]
 for y, hpw in zip(yearlyList,weekHours):
     hourly = calcu(y, hpw)
-    print("yearly: ", y, "hoursPerWeek: ", hpw, "monthly :", y/12, "hourly :", hourly )
+    print("yearly: ", y, "hoursPerWeek: ", hpw, "monthly :", round(y/12, 2), "hourly :", round(hourly, 2) )
